7cfbe8e80a1c2e99fae4cfa4996550450850b301
Rob Inglis (Group) <Rob.Inglis@dimensiondata.com>
2019-01-09 11:10:48 +0200

    Initial Upload

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>Revenue Marketing</title>
+    <meta charset="utf-8">
+  </head>
+
+  <body>
+
+    <h1>Revenue Marketing: Coming Soon</h1>
+    <h2>Rob Inglis <?php echo date('Y'); ?> </h2>
+
+  </body>
+</html>
